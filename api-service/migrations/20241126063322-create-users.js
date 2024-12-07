7bbda8acc21e39b262494d144d11ed167e70db96
masthansaginala <163691600+masthansaginala@users.noreply.github.com>
2024-12-07 13:30:45 +0000

    Added User Model and Migration

---
+++ api-service/migrations/20241126063322-create-users.js
@@ -0,0 +1,98 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Users', {
+      user_id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true,
+      },
+      user_first_name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      user_last_name: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      user_date_of_birth: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+      user_gender: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      user_email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      user_phone_number: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      user_address_line_one: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      user_address_line_two: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      user_city: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      user_state: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      user_country: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      user_zipcode: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      user_role: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      user_password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      user_pin: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      user_status: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.NOW
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+      deleted_at: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Users');
+  },
+};
+
