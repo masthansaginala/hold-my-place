8d088fdcd9ec746e3cd32036d48118b93a7ea7f0
masthansaginala <163691600+masthansaginala@users.noreply.github.com>
2024-12-11 08:53:57 +0000

    Updated index route file for event and booking

--- api-service/routes/index.js
+++ api-service/routes/index.js
@@ -2,13 +2,17 @@ const express = require('express');
 const userRoutes = require('./user');
 const organizerRoutes = require('./organizer');
 const vendorRoutes = require('./vendor');
-const vendorBusinessRoutes = require('./vendor_business')
+const vendorBusinessRoutes = require('./vendor_business');
+const eventRoutes = require('./event');
+const bookingRoutes = require('./booking');
 
 const router = express.Router();
 
 router.use('/users', userRoutes);
 router.use('/organizers', organizerRoutes);
-router.use('/vendors', vendorRoutes)
-router.use('/vendor-business', vendorBusinessRoutes)
+router.use('/vendors', vendorRoutes);
+router.use('/vendor-business', vendorBusinessRoutes);
+router.use('/events', eventRoutes);
+router.use('/booking', bookingRoutes);
 
 module.exports = router;
